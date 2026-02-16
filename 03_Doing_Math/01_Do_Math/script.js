86410f92cdad0cb4956197ae99630ea460bebcc6
Julio Cesar Bernal Mendez <juliocesarbernalmendez@hotmail.com>
2026-02-16 16:38:57 -0600

    Add Do_Math

---
+++ 03_Doing_Math/01_Do_Math/script.js
@@ -0,0 +1,8 @@
+var apples = 5, pears = 10;
+var piecesOfFruit = apples + pears;
+alert("Pieces of fruit: " + piecesOfFruit);
+
+var piecesForEachPerson = piecesOfFruit / 3;
+alert("Pieces for each person: " + piecesForEachPerson)
+
+alert("(10 + 2) / 2 + 4 * 2 = " + String((10 + 2) / 2 + 4 * 2))
