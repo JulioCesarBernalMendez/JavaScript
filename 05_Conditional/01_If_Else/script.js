a8e388b971179015a926fcdfbadffac3e55bd8d3
Julio Cesar Bernal Mendez <juliocesarbernalmendez@hotmail.com>
2026-02-16 18:22:26 -0600

    Add If_Else

---
+++ 05_Conditional/01_If_Else/script.js
@@ -0,0 +1,23 @@
+/* This is a ...
+   ... multiline comment */
+
+// this is a single line comment
+
+if (10 > 5)
+{
+    alert("Running code 10 > 5")
+}
+
+if (10 < 5)
+{
+    alert("Running code 10 < 5")
+}
+
+if (43 < 2)
+{
+    alert("Running code 43 < 2")
+}
+else
+{
+    alert("Running code in else")
+}
