a33e7ccd49ca1dfee7f9478c68322c86377e9fd0
Julio Cesar Bernal Mendez <juliocesarbernalmendez@hotmail.com>
2026-02-16 20:25:09 -0600

    Add Function

---
+++ 07_Functions/01_Function/script.js
@@ -0,0 +1,10 @@
+var add = function (a, b)
+{
+    return a + b;
+}
+
+var val1 = Number(prompt("Enter a number"));
+var val2 = Number(prompt("Enter another number"));
+var sum = add(val1, val2);
+
+alert("sum is: " + String(sum));
